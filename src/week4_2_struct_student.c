3b21b6cfbee1814bdaf370383d5fc06fe3b0a579
Enesyildirim2 <134625875+Enesyildirim2@users.noreply.github.com>
2025-10-06 08:28:51 +0000

    lab4

--- src/week4_2_struct_student.c
+++ src/week4_2_struct_student.c
@@ -1,7 +1,7 @@
 /*
  * week4_2_struct_student.c
- * Author: [Your Name]
- * Student ID: [Your ID]
+ * Author: ENES YILDIRIM
+ * Student ID: 211ADB121
  * Description:
  *   Demonstrates defining and using a struct in C.
  *   Students should define a 'Student' struct with fields like name, id, and grade.
@@ -13,18 +13,27 @@
 
 // TODO: Define struct Student with fields: name (char[]), id (int), grade (float)
 // Example:
-// struct Student {
-//     char name[50];
-//     int id;
-//     float grade;
-// };
+ struct Student {
+     char name[50];
+     int id;
+     float grade;
+ };
 
 int main(void) {
     // TODO: Declare one or more Student variables
+    struct Student student1;
+    struct Student student2;
 
     // TODO: Assign values (either manually or via scanf)
+    strcpy(student1.name, "Enes");
+    student1.id = 211121;
+    student1.grade = 75.2;
+    strcpy(student2.name, "Jack");
+    student2.id = 123124;
+    student2.grade = 79.9;
 
     // TODO: Print struct contents using printf
-
+    printf("%s %d %.2f\n", student1.name, student1.id, student1.grade);
+    printf("%s %d %.2f\n", student2.name, student2.id, student2.grade);
     return 0;
 }
--- src/week4_3_struct_database.c
+++ src/week4_3_struct_database.c
@@ -13,6 +13,11 @@
 #include <string.h>
 
 // TODO: Define struct Student with fields name, id, grade
+struct Student {
+    char name[100];
+    int id;
+    float grade;
+};
 
 int main(void) {
     int n;
@@ -25,14 +30,33 @@ int main(void) {
     }
 
     // TODO: Allocate memory for n Student structs using malloc
+    students = malloc(n * sizeof(struct Student));
+    
 
     // TODO: Read student data in a loop
+    if (students == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("Enter name, id and grade for student %d: ", i + 1);
+        scanf("%s %d %f", students[i].name, &students[i].id, &students[i].grade);
+    }
+
 
     // TODO: Display all student records in formatted output
-
+    printf("records:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s %d %.2f\n", students[i].name, students[i].id, students[i].grade);
+    }
     // Optional: Compute average grade or find top student
-
+    float t = 0.0;
+    for (int i = 0; i < n; i++) {
+        t += students[i].grade;
+    }
+    float average = t / n;
+    printf("average: %.2f\n", average);
     // TODO: Free allocated memory
-
+    free(students);
     return 0;
 }
